d84cee53fa7e92a3e8f48405f9b5234a28a454b9
Moritz Wieland <moritz.wieland@googlemail.com>
2025-10-30 12:50:33 +0100

    feat(backend): introduce config module/service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import Joi from "joi";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                PORT: Joi.number().port().required(),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
