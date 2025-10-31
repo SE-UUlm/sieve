775101c4ea5298ddaec27d1065406e89fb3795cb
Moritz Wieland <moritz.wieland@googlemail.com>
2025-10-31 16:08:09 +0100

    feat(backend): add database integration to backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,14 +1,35 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import Joi from "joi";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
     imports: [
         ConfigModule.forRoot({
+            isGlobal: true,
             validationSchema: Joi.object({
-                PORT: Joi.number().port().required(),
+                BACKEND_PORT: Joi.number().port().required(),
+                DB_HOST: Joi.string().default("localhost"),
+                DB_PORT: Joi.number().port().default(5432),
+                DB_USERNAME: Joi.string().default("postgres"),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().default("postgres"),
+            }),
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: "postgres",
+                host: configService.get<string>("DB_HOST"),
+                port: configService.get<number>("DB_PORT"),
+                username: configService.get<string>("DB_USERNAME"),
+                password: configService.get<string>("DB_PASSWORD"),
+                database: configService.get<string>("DB_NAME"),
+                entities: [],
+                autoLoadEntities: true,
             }),
         }),
     ],
