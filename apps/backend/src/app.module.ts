e847a8339b6af06faf9e01a5abdfce6a9903d20b
Moritz Wieland <moritz.wieland@googlemail.com>
2025-11-06 15:40:18 +0100

    refactor: extract config module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import Joi from "joi";
 import { DatabaseModule } from "./database/database.module";
+import { AppConfigModule } from "./config/config.module";
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            validationSchema: Joi.object({
-                BACKEND_PORT: Joi.number().port().required(),
-
-                DB_HOST: Joi.string().default("localhost"),
-                DB_PORT: Joi.number().port().default(5432),
-                DB_USERNAME: Joi.string().default("postgres"),
-                DB_PASSWORD: Joi.string().required(),
-                DB_NAME: Joi.string().default("postgres"),
-            }),
-        }),
-        DatabaseModule,
-    ],
+    imports: [AppConfigModule, DatabaseModule],
     controllers: [AppController],
     providers: [AppService],
 })
