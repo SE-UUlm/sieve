cdca7ec5f60462391b317e8fd33577a083e79be8
Moritz Wieland <moritz.wieland@googlemail.com>
2025-11-07 14:52:53 +0100

    refactor(backend): refactor DTOs using PickType utility

--- apps/backend/src/email/dto/create-email.dto.ts
+++ apps/backend/src/email/dto/create-email.dto.ts
@@ -1,39 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { PickType } from "@nestjs/swagger";
+import { EmailDto } from "./email.dto";
 
-export class CreateEmailDto {
-    @ApiProperty({
-        description: "Sender of the email",
-        type: String,
-        example: "alice.smith@example.com",
-        required: false,
-        nullable: true,
-    })
-    @IsOptional()
-    @IsEmail()
-    sender?: string | null;
-
-    @ApiProperty({
-        description: "Subject of the email",
-        type: String,
-        example: "Piece Availability Request",
-        required: false,
-        nullable: true,
-    })
-    @IsOptional()
-    @IsString()
-    subject?: string | null;
-
-    @ApiProperty({
-        description: "Body of the email",
-        type: String,
-        example:
-            "Dear Sir or Madam,\n\n" +
-            "I would like to request information regarding the availability of the following pieces...\n\n" +
-            "Best regards,\n" +
-            "Alice Smith",
-    })
-    @IsString()
-    @IsNotEmpty()
-    body!: string;
-}
+export class CreateEmailDto extends PickType(EmailDto, ["sender", "subject", "body"] as const) {}
--- apps/backend/src/user/dto/simple-user.dto.ts
+++ apps/backend/src/user/dto/simple-user.dto.ts
@@ -1,28 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { UserRole } from "../entities/user.entity";
-import { IsEmail, IsEnum, IsNotEmpty, IsString, IsUUID } from "class-validator";
-import { EXAMPLE_USER_ID } from "../../common/examples.constants";
+import { PickType } from "@nestjs/swagger";
+import { UserDto } from "./user.dto";
 
-export class SimpleUserDto {
-    // TODO: replace with actual UUID version once implemented
-    @ApiProperty({ description: "Unique user ID", type: String, example: EXAMPLE_USER_ID })
-    @IsUUID()
-    id!: string;
-
-    @ApiProperty({ description: "Full name of the user", type: String, example: "John Doe" })
-    @IsString()
-    @IsNotEmpty()
-    name!: string;
-
-    @ApiProperty({
-        description: "Email address of the user",
-        type: String,
-        example: "john.doe@example.com",
-    })
-    @IsEmail()
-    email!: string;
-
-    @ApiProperty({ description: "Role of the user", enum: UserRole, example: UserRole.USER })
-    @IsEnum(UserRole)
-    role!: UserRole;
-}
+export class SimpleUserDto extends PickType(UserDto, ["id", "name", "email", "role"] as const) {}
