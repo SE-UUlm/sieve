7b383c4c99e9654f9cca2a67ff1baa4d268ac571
Moritz Beck (Birkenstab) <git@birkenstab.de>
2025-11-12 17:14:53 +0100

    feat(frontend): Add Login Form (WIP)

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import AuthForm from "@/components/auth/AuthForm";
 import Hero from "./_components/Hero";
 
 export default function Home() {
     return (
-        <main className="flex flex-col p-4 lg:p-24">
+        <main className="flex flex-col p-4 lg:p-24 2xl:flex-row">
             <Hero />
+            <AuthForm />
         </main>
     );
 }
---
+++ apps/frontend/src/components/auth/LoginForm.tsx
@@ -0,0 +1,112 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { CSSProperties, useId } from "react";
+
+const formSchema = z.object({
+    username: z
+        .string()
+        .min(3, "Username must be at least 3 characters.")
+        .max(32, "Username must be at most 32 characters."),
+    password: z
+        .string()
+        .min(3, "Password must be at least 3 characters.")
+        .max(100, "Password must be at most 100 characters."),
+});
+
+const LoginForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    function onSubmit(data: z.infer<typeof formSchema>) {
+        toast("You submitted the following values:", {
+            description: (
+                <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+                    <code>{JSON.stringify(data, null, 2)}</code>
+                </pre>
+            ),
+            position: "bottom-right",
+            classNames: {
+                content: "flex flex-col gap-2",
+            },
+            style: {
+                "--border-radius": "calc(var(--radius)  + 4px)",
+            } as CSSProperties,
+        });
+    }
+
+    const formId = useId();
+    const usernameId = useId();
+    const passwordId = useId();
+
+    return (
+        <Card className="mx-auto w-full sm:max-w-md">
+            <CardHeader>
+                <CardTitle>Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form id={formId} onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup>
+                        <Controller
+                            name="username"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor={usernameId}>Username</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        id={usernameId}
+                                        aria-invalid={fieldState.invalid}
+                                        autoComplete="username"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid}>
+                                    <FieldLabel htmlFor={passwordId}>Password</FieldLabel>
+                                    <Input
+                                        {...field}
+                                        type="password"
+                                        id={passwordId}
+                                        aria-invalid={fieldState.invalid}
+                                        autoComplete="current-password"
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                    </FieldGroup>
+                </form>
+            </CardContent>
+            <CardFooter>
+                <Field orientation="horizontal">
+                    <Button type="submit" form={formId}>
+                        Login
+                    </Button>
+                </Field>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default LoginForm;
