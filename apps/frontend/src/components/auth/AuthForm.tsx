100c32ca0fa338f1bc0cb003a213c85be426bfa2
Moritz Beck (Birkenstab) <git@birkenstab.de>
2025-11-12 17:14:53 +0100

    feat(frontend): Add Login Form (WIP)

---
+++ apps/frontend/src/components/auth/AuthForm.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LoginForm from "./LoginForm";
+
+const AuthForm = () => {
+    return (
+        <>
+            <LoginForm />
+        </>
+    );
+};
+
+export default AuthForm;
