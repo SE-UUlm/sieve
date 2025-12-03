b83d3990365eed47fa165b3117ef00438fc94e04
Moritz Wieland <moritz.wieland@googlemail.com>
2025-12-03 23:17:55 +0100

    feat(backend): make /health route available to everyone

--- apps/backend/src/health/health.controller.ts
+++ apps/backend/src/health/health.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from "@nestjs/common";
 import { HealthCheck, HealthCheckService, PrismaHealthIndicator } from "@nestjs/terminus";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { PrismaService } from "../prisma/prisma.service";
+import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
 
 @ApiTags("Health")
 @Controller("health")
@@ -13,6 +14,7 @@ export class HealthController {
     ) {}
 
     @Get()
+    @AllowAnonymous()
     @ApiOperation({ summary: "Gets the health status of the SIEVE backend service" })
     @HealthCheck()
     check() {
