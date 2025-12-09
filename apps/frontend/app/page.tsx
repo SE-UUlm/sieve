e97bdff6685a22e407d8e5afe6640597173cc275
Moritz Beck (Birkenstab) <git@birkenstab.de>
2025-12-09 17:39:03 +0100

    feat(frontend): only show login buttons on startpage when not logged in

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,23 +1,36 @@
-import AuthForm from "@/components/auth/AuthForm";
+"use client";
 import Hero from "./_components/Hero";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import Link from "next/link";
 
 export default function Home() {
+    const { data: session } = authClient.useSession();
+
     return (
         <main className="flex flex-col p-4 lg:p-24">
             <Hero />
             <div className="mx-auto flex gap-4">
-                <Button variant="default" asChild size={"lg"}>
-                    <Link href="/auth/signup" className="text-xl">
-                        Sign up for Sieve
-                    </Link>
-                </Button>
-                <Button variant="outline" asChild size={"lg"}>
-                    <Link href="/auth/login" className="text-xl">
-                        Login
-                    </Link>
-                </Button>
+                {session?.user ? (
+                    <Button variant="default" asChild size={"lg"}>
+                        <Link href="/analyse" className="text-xl">
+                            Start Analysing Emails
+                        </Link>
+                    </Button>
+                ) : (
+                    <>
+                        <Button variant="default" asChild size={"lg"}>
+                            <Link href="/auth/signup" className="text-xl">
+                                Sign up for Sieve
+                            </Link>
+                        </Button>
+                        <Button variant="outline" asChild size={"lg"}>
+                            <Link href="/auth/login" className="text-xl">
+                                Login
+                            </Link>
+                        </Button>
+                    </>
+                )}
             </div>
         </main>
     );
