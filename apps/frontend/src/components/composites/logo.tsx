60ea391f4b80508aa7a311337e613b7cb0f202f2
Moritz Wieland <moritz.wieland@googlemail.com>
2026-02-06 15:40:34 +0100

    feat(frontend): replace css with svg logo

--- apps/frontend/src/components/composites/logo.tsx
+++ apps/frontend/src/components/composites/logo.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import sieveLogo from "../../../public/sieve-logo.svg";
+
 export function Logo() {
     return (
-        <div className="mb-10">
-            <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-blue-600 text-xl font-bold text-white shadow-lg shadow-blue-900/20">
-                S
-            </div>
-        </div>
+        <Link
+            href="/analyze"
+            className="mb-10 block"
+            aria-label="Go to Analyze"
+        >
+            <Image src={sieveLogo} alt="Sieve Logo" />
+        </Link>
     );
 }
