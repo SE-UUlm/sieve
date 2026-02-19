e37261b11e0bb89830846eecf19d961befaa3f5c
Moritz Beck (Birkenstab) <git@birkenstab.de>
2026-02-19 19:15:51 +0100

    feat(ai-backend): Add product search with lego set from a txt file

--- apps/ai-backend/src/ai_backend/agent.py
+++ apps/ai-backend/src/ai_backend/agent.py
@@ -1,32 +1,61 @@
+import json
+from langchain.tools import tool
 import pprint
 from langchain.agents import create_agent
-from langchain.messages import HumanMessage
+from langchain.messages import HumanMessage, SystemMessage
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
-from ai_backend.schemas import ResponseFormat, ResponseFormatData
+from ai_backend.schemas import ResponseFormat, ResponseFormatData, SearchResult
 
 load_dotenv()
 
 
-system_prompt = """Your job is to categorize a given email from a customer or potential customer and convert it into a structured form.
-    If none of the categories match, classify the email as 'Other'.
-    Write answers in the third person about the customer. Be as concise as possible.
-    Answer Language: English"""
+@tool
+def search_product(query: str):
+    """Useful to search for product information, product categories. Use one keyword for search. The database only contains lego sets. The products in the database are named in german"""
+    with open("legosets.txt") as f:
+        s = f.read()
+        lines = s.split("\n")
+        keywords = query.split(" ")
+        keywords = [x.strip().lower() for x in keywords]
+        keywords = [x for x in keywords if x != "lego"]
+        filtered = [x for x in lines if any(k in x.lower() for k in keywords)]
+        print(f"🛠️ search_product: {query} -> {filtered}")
+        return filtered
 
 
-def _build_agent(api_key: str):
-    model = init_chat_model(
-        model="gpt-4o-mini",
+def _build_model(api_key: str):
+    return init_chat_model(
+        model="gpt-5.2",
         temperature=0.1,
         timeout=10,
         max_tokens=10000,
         api_key=api_key,
     )
 
+
+def _build_response_agent(model):
+    system_prompt = """Your job is to categorize a given email from a customer or potential customer and convert it into a structured form.
+    If none of the categories match, classify the email as 'Other'.
+    Write answers in the third person about the customer. Be as concise as possible.
+    Answer Language: English"""
     return create_agent(
         model=model,
         system_prompt=system_prompt,
         response_format=ResponseFormat,
+    )
+
+
+def _build_search_agent(model):
+    system_prompt = """Your job is to find the product(s) the customer wants to buy or is talking about in their email. 
+    Use the search_product tool to find the products. Try multiple times with different keywords, variants, translations until you think you found the products the customer wants. 
+    If you think you found the right products return them using the provided output format. Together with your confidence score.
+    Answer Language: English"""
+    return create_agent(
+        model=model,
+        system_prompt=system_prompt,
+        response_format=SearchResult,
+        tools=[search_product],
     )
 
 
@@ -40,9 +69,24 @@ def _build_email_for_analysis(subject: str | None, body: str) -> str:
 async def run_analyze_email_agent(
     api_key: str, subject: str | None, body: str
 ) -> ResponseFormatData:
-    agent = _build_agent(api_key=api_key)
     formatted_email = _build_email_for_analysis(subject=subject, body=body)
+
+    model = _build_model(api_key)
+
+    search_agent = _build_search_agent(model)
     conversation = [HumanMessage(formatted_email)]
-    result = await agent.ainvoke({"messages": conversation})
+    result = await search_agent.ainvoke({"messages": conversation})
+    pprint.pp(result)
+
+    print("🔎 search agent result:\n" + result["structured_response"].model_dump_json())
+
+    response_agent = _build_response_agent(model)
+    conversation = [
+        SystemMessage(
+            "Related products: \n" + result["structured_response"].model_dump_json()
+        ),
+        HumanMessage(formatted_email),
+    ]
+    result = await response_agent.ainvoke({"messages": conversation})
     pprint.pp(result)
     return result["structured_response"].data
--- apps/ai-backend/src/ai_backend/schemas.py
+++ apps/ai-backend/src/ai_backend/schemas.py
@@ -20,17 +20,24 @@ class Complaint(BaseModel):
 class Product(BaseModel):
     product_name: str
     quantity: int
+    product_id: str = Field(description="If not known, leave empty")
+    product_category: str = Field(description="If not known, leave empty")
+    number_of_parts: int = Field(description="If not known, leave empty")
 
 
 class ProductInquiry(BaseModel):
-    """The user wants to buy a product or asks for general product information."""
+    """The user wants to buy a product or asks for general product information. Use the provided 'related products' to fill out the products list"""
 
     category: Literal["Product_Inquiry"]
     products: list[Product]
+    question: str | None
+    answer: str | None = Field(
+        description="If the customer asked a question and you can answer the question based on the provided product details, then answer here"
+    )
 
 
 class Issue(BaseModel):
-    product_name: str
+    product: Product
     issue: str = Field(description="A short summary of the issue")
 
 
@@ -51,3 +58,12 @@ ResponseFormatData = Union[
 
 class ResponseFormat(BaseModel):
     data: ResponseFormatData
+
+
+class SearchResult(BaseModel):
+    potentialProducts: list[Product] = Field(
+        description="Products from the search you think are the ones the customer wanted"
+    )
+    confidence: float = Field(
+        description="How confident you are that the products are the ones the customer wanted"
+    )
