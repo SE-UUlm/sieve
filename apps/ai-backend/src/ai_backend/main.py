87de81e031be4c4f968d46ccf9e29687d92165e7
Moritz Beck (Birkenstab) <git@birkenstab.de>
2026-02-25 16:06:23 +0100

    fix(ai-backend): only connect to db when a request is made

--- apps/ai-backend/src/ai_backend/main.py
+++ apps/ai-backend/src/ai_backend/main.py
@@ -1,14 +1,12 @@
 import os
 import asyncpg
-from fastapi.concurrency import asynccontextmanager
 from ai_backend.agent import run_analyze_email_agent
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.db = await asyncpg.create_pool(
+def create_pool():
+    pool = asyncpg.create_pool(
         user=os.environ.get("PRODUCT_DB_USERNAME"),
         password=os.environ.get("PRODUCT_DB_PASSWORD"),
         database=os.environ.get("PRODUCT_DB_NAME"),
@@ -19,11 +17,7 @@ async def lifespan(app: FastAPI):
     )
     print("Database pool created")
 
-    yield
-
-    # Shutdown
-    await app.state.db.close()
-    print("Database pool closed")
+    return pool
 
 
 class Email(BaseModel):
@@ -32,7 +26,7 @@ class Email(BaseModel):
     body: str = Field(..., min_length=1, max_length=10000)
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 
 @app.get("/")
@@ -42,10 +36,12 @@ def read_root():
 
 @app.post("/analyze-email")
 async def analyze_email(email: Email):
-    result = await run_analyze_email_agent(
-        api_key=email.apiKey,
-        subject=email.subject,
-        body=email.body,
-        db_pool=app.state.db,
-    )
-    return {"status": "success", "data": result}
+    # Use a separate pool for each request to allow dynamic credentials from backend in the future
+    async with create_pool() as pool:
+        result = await run_analyze_email_agent(
+            api_key=email.apiKey,
+            subject=email.subject,
+            body=email.body,
+            db_pool=pool,
+        )
+        return {"status": "success", "data": result}
