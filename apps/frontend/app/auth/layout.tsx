1e3d35a36896653a378f9a050089061b35bd3f4e
Moritz Wieland <moritz.wieland@googlemail.com>
2026-03-15 15:58:30 +0100

    chore(frontend): add missing imports

--- apps/frontend/app/auth/layout.tsx
+++ apps/frontend/app/auth/layout.tsx
@@ -1,3 +1,5 @@
+import type React from "react";
+
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="min-h-screen bg-slate-50 transition-colors duration-300 dark:bg-slate-950">
