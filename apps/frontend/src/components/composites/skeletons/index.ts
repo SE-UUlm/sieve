a62d63d6ea1073aaa11425cb25fcab4513abd73f
Moritz Wieland <moritz.wieland@googlemail.com>
2026-03-18 11:37:07 +0100

    feat(frontend): add reusable skeleton infrastructure

---
+++ apps/frontend/src/components/composites/skeletons/index.ts
@@ -0,0 +1,7 @@
+export { FormSkeleton } from "./form-skeleton";
+export { ListItemSkeleton } from "./list-item-skeleton";
+export { ListSkeleton } from "./list-skeleton";
+export { NavigationRailSkeleton } from "./navigation-rail-skeleton";
+export { SkeletonCard } from "./skeleton-card";
+export { SplitViewSkeleton } from "./split-view-skeleton";
+export { TextBlockSkeleton } from "./text-block-skeleton";
